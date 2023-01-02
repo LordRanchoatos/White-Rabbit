be7381c4c110a6aff837ad1a7104fe3db34e6c1e
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-02 22:02:06 +0100

    applied eslint

--- src/app/events/Welcome.ts
+++ src/app/events/Welcome.ts
@@ -3,24 +3,27 @@ import Log, { LogUtils } from '../utils/Log';
 import { TextChannel } from 'discord.js';
 import { DiscordEvent } from '../types/discord/DiscordEvent';
 
-const channelId = "997872691572916309";
+const channelId = '997872691572916309';
 export default class implements DiscordEvent {
-    name = 'ready';
-    once = true;
+  name = 'ready';
+  once = true;
 
+//   functions on entry to the server 
+// welcome message. 
+// send option to select project.
+// asign role tag.
 
-    async execute(client: Client): Promise<any> {
-        try {
-            client.on("guildMemberAdd", (member) => {
-                console.log(member)
-                const message = `Welcome to the garage, Let's get you started <@${member.id}>`
-                const channel = member.guild.channels.cache.get(channelId) as TextChannel;
-                channel.send(message)
-
-            })
-        } catch (e) {
-            LogUtils.logError('Error processing event ready', e);
-        }
-
+  async execute(client: Client): Promise<any> {
+    try {
+      client.on('guildMemberAdd', (member: any) => {
+        // console.log(member);
+        const message = `Welcome to the garage, Let's get you started <@${member.id}>`;
+        const channel = member.guild.channels.cache.get(channelId) as TextChannel;
+        channel.send(message);
+      });
+    } catch (e) {
+      LogUtils.logError('Error processing event ready', e);
     }
+
+  }
 }
