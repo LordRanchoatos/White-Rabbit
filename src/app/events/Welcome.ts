2bde6e44530ac3990b7689bce63aec4497f06e0e
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-02 23:17:30 +0100

    add the explorer role

--- src/app/events/Welcome.ts
+++ src/app/events/Welcome.ts
@@ -3,24 +3,29 @@ import Log, { LogUtils } from '../utils/Log';
 import { TextChannel } from 'discord.js';
 import { DiscordEvent } from '../types/discord/DiscordEvent';
 
-const channelId = '997872691572916309';
+const channelId = '997872691572916306';
 export default class implements DiscordEvent {
   name = 'ready';
   once = true;
 
-//   functions on entry to the server 
-// welcome message. 
-// send option to select project.
-// asign role tag.
+  // functions on entry to the server 
+  // welcome message: 
+  // 1. automatically asign a role on joining. 
+  // send option to select project, 
+  // asign role tag.
 
   async execute(client: Client): Promise<any> {
     try {
       client.on('guildMemberAdd', (member: any) => {
         // console.log(member);
-        const message = `Welcome to the garage, Let's get you started <@${member.id}>`;
+        const message = `Welcome to the garage, Let's get you started <@${member.id}>, You will have been assigned the Explorer role.`;
         const channel = member.guild.channels.cache.get(channelId) as TextChannel;
         channel.send(message);
+        const welcomeRole = member.guild.roles.cache.find(role => role.name === 'Explorer');
+        member.roles.add(welcomeRole);
+        // member.guild.channels.cache.get('YOU_CHANNEL_ID').send(`Welcome <@${member.user.id}> to our server! Make sure to check out the rules channel!`)
       });
+    
     } catch (e) {
       LogUtils.logError('Error processing event ready', e);
     }
