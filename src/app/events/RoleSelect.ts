1a7e0f635470167750726e1246189a4de9ef13e2
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-05 10:50:48 +0100

    fixed error

--- src/app/events/RoleSelect.ts
+++ src/app/events/RoleSelect.ts
@@ -1,6 +1,6 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, Guild } from 'discord.js';
 import Log, { LogUtils } from '../utils/Log';
-import { TextChannel } from 'discord.js';
+import { GuildMemberRoleManager } from 'discord.js';
 import { DiscordEvent } from '../types/discord/DiscordEvent';
 
 const ROLES = {
@@ -11,58 +11,56 @@ const ROLES = {
 
 
 export default class implements DiscordEvent {
-    name = 'ready';
-    once = false;
+    name = 'interactionCreate';
+    once = true;
 
-    async execute(client: Client): Promise<any>{
+    async execute(interaction, client): Promise<any>{
 
-        client.on('interactionCreate', async (interaction) => {
-        if (interaction.isButton()) {
-        const role = interaction.guild.roles.cache.get(
-            ROLES[interaction.customId.toUpperCase()] // the button's custom Id MUST match your ROLES property defined above
-        );
+            if (interaction.isButton()) {
+                const role = interaction.guild.roles.cache.get(
+                    ROLES[interaction.customId.toUpperCase()] // the button's custom Id MUST match your ROLES property defined above
+                );
     
-        if (!role)
-            return interaction.reply({ content: 'Role not found', ephemeral: true });
+                if (!role) return interaction.reply({ content: 'Role not found', ephemeral: true });
     
-        const hasRole = interaction.member.roles.cache.has(role.id);
-        console.log(hasRole);
+                const hasRole = interaction.member.roles.cache.has(role.id);
+                console.log(hasRole);
     
-        if (hasRole)
-            return interaction.member.roles
-            .remove(role)
-            .then((member) =>
-                interaction.reply({
-                content: `The ${role} role was removed to you ${member}`,
-                ephemeral: true,
-                })
-            )
-            .catch((err) => {
-                console.log(err);
-                return interaction.reply({
-                content: `Something went wrong. The ${role} role was not removed to you ${member}`,
-                ephemeral: true,
+            if (hasRole)
+                return interaction.member.roles
+                .remove(role)
+                .then((member) =>
+                    interaction.reply({
+                    content: `The ${role} role was removed to you ${member}`,
+                    ephemeral: true,
+                    })
+                )
+                .catch((err, member) => {
+                    console.log(err);
+                    return interaction.reply({
+                    content: `Something went wrong. The ${role} role was not removed to you ${member}`,
+                    ephemeral: true,
+                    });
                 });
-            });
-        else
-            return interaction.member.roles
-            .add(role)
-            .then((member) =>
-                interaction.reply({
-                content: `The ${role} role was added to you ${member}`,
-                ephemeral: true,
-                })
-            )
-            .catch((err) => {
-                console.log(err);
-                return interaction.reply({
-                content: `Something went wrong. The ${role} role was not added to you ${member}`,
-                ephemeral: true,
+            else
+                return interaction.member.roles
+                .add(role)
+                .then((member) =>
+                    interaction.reply({
+                    content: `The ${role} role was added to you ${member}`,
+                    ephemeral: true,
+                    })
+                )
+                .catch((err, member) => {
+                    console.log(err);
+                    return interaction.reply({
+                    content: `Something went wrong. The ${role} role was not added to you ${member}`,
+                    ephemeral: true,
+                    });
                 });
-            });
+            }
     }
-  }
-);
+};
 
 
 
@@ -124,5 +122,5 @@ export default class implements DiscordEvent {
         // } catch (e) {
         //     LogUtils.logError('Error processing event ready', e);
         // }
-    }
+    //}
 }
