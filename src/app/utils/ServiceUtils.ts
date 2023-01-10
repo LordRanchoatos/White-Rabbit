5f24de01c08e54161b250b542070dcb5aa0c177b
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-10 21:58:46 +0100

    clean up serviceUtils

--- src/app/utils/ServiceUtils.ts
+++ src/app/utils/ServiceUtils.ts
@@ -2,28 +2,10 @@
  * Utilities for service layer
  */
 import {
-  AwaitMessagesOptions,
-  Collection,
-  DMChannel,
-  Guild,
-  GuildMember,
-  Permissions,
   Role,
   RoleManager,
-  Snowflake,
-  StageChannel,
-  User,
-  VoiceChannel,
 } from 'discord.js';
-import { Db } from 'mongodb';
-import { ButtonStyle, CommandContext, ComponentActionRow, ComponentType } from 'slash-create';
-import client from '../app';
-import ValidationError from '../errors/ValidationError';
-import constants from '../service/constants/constants';
 import roleIds from '../service/constants/roleIds';
-import discordServerIds from '../service/constants/discordServerIds';
-import Log, { LogUtils } from './Log';
-import MongoDbUtils from '../utils/MongoDbUtils';
 
 
 const ServiceUtils = {
