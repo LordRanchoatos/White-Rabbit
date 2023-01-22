134e57eaf8e2508366ad092a7dfd5cfc438d3cee
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-22 03:54:08 +0100

    fixing mhlauncher

--- src/app/commands/Role/MhLauncher.ts
+++ src/app/commands/Role/MhLauncher.ts
@@ -9,7 +9,6 @@ import client from '../../App';
 import roleIds from '../../service/constants/roleIds';
 import { addmhlauncherToUser } from '../../service/role/Addmhlauncher';
 import ServiceUtils from '../../utils/ServiceUtils';
-import ValidationError from '../../errors/ValidationError';
 import discordServerIds from '../../service/constants/discordServerIds';
 import Log, { LogUtils } from '../../utils/Log';
 import { command } from '../../utils/SentryUtils';
@@ -22,7 +21,7 @@ export default class Coordinape extends SlashCommand {
       guildIDs: [discordServerIds.kingPin, discordServerIds.discordBotGarage],
       options: [
         {
-          type: CommandOptionType.MENTIONABLE,
+          type: CommandOptionType.SUB_COMMAND_GROUP,
           name: 'bot',
           description: 'Grant mhlauncher role to:',
           required: true,
@@ -32,32 +31,15 @@ export default class Coordinape extends SlashCommand {
         usages: 2,
         duration: 1,
       },
-      defaultPermission: false,
-      permissions: {
-        [discordServerIds.kingPin]: [
-          {
-            type: ApplicationCommandPermissionType.ROLE,
-            id: roleIds.mhstarter,
-            permission: true,
-          },
-        ],
-        [discordServerIds.discordBotGarage]: [
-          {
-            type: ApplicationCommandPermissionType.ROLE,
-            id: roleIds.mhstarter,
-            permission: true,
-          },
-        ],
-      },
+      defaultPermission: true,
     });
   }
-
-	@command
+@command
   async run(ctx: CommandContext): Promise<any> {
     LogUtils.logCommandStart(ctx);
     if (ctx.user.bot) return;
 		
-    Log.info('/mad hatter Launcher start');
+    Log.info('/mhlauncher start');
     const guild = await client.guilds.fetch(ctx.guildID);
     const starter = await guild.members.fetch(ctx.user.id);
     const botId = await guild.members.fetch(ctx.options.id);
